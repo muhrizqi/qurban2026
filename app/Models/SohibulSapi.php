b40348e8aa49ba129d775de1abe7d0e3c124a5f9
muhrizqi <muh.rizqi@gmail.com>
2026-05-05 10:16:25 +0700

    Correct Filament and Activitylog versions to fix method-missing and trait errors

--- app/Models/SohibulSapi.php
+++ app/Models/SohibulSapi.php
@@ -4,12 +4,12 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
-use Spatie\Activitylog\Models\Concerns\HasActivity;
+use Spatie\Activitylog\Traits\LogsActivity;
 use Spatie\Activitylog\LogOptions;
 
 class SohibulSapi extends Model
 {
-    use HasActivity;
+    use LogsActivity;
 
     public function getActivitylogOptions(): LogOptions
     {
